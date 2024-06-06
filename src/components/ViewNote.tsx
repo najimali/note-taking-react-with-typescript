00be8ad288e209f53c0c3f96c501c0c96097fe34
najimali <najim.ali32@gmail.com>
2024-06-06 12:34:19 +0530

    Added Delete Note Functionality

--- src/components/ViewNote.tsx
+++ src/components/ViewNote.tsx
@@ -1,12 +1,31 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
-import { Link, Navigate, useParams } from "react-router-dom";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 import ReactMarkDown from "react-markdown";
 import useNote from "../hooks/useNote";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { RawNote } from "../types/Note";
+import { localStorageKeys } from "../utils/constants";
+import { useEffect, useState } from "react";
 const ViewNote = () => {
   const { id } = useParams();
   const note = useNote(id);
+  const navigate = useNavigate();
+  const [shouldNavigate, setShouldNavigate] = useState(false);
+  const [, setNotes] = useLocalStorage<RawNote[]>(localStorageKeys.Notes, []);
+  useEffect(() => {
+    if (shouldNavigate) {
+      navigate("/");
+    }
+  }, [shouldNavigate, navigate]);
   if (!note) return <Navigate to="/" replace></Navigate>;
+
+  const onDelete = (id: string | undefined) => {
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+    setShouldNavigate(true);
+  };
+
   const { title, markdown, tags } = note;
+
   return (
     <>
       <Row className="items-center mb-4">
@@ -27,7 +46,9 @@ const ViewNote = () => {
             <Link to={`/${id}/edit`}>
               <Button variant="primary">Edit</Button>
             </Link>
-            <Button variant="outline-danger">Delete</Button>
+            <Button variant="outline-danger" onClick={() => onDelete(id)}>
+              Delete
+            </Button>
             <Link to="/">
               <Button variant="outline-secondary">Back</Button>
             </Link>
